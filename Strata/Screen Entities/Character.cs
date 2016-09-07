b98684eca099e75dfd51493e0924cf6dd0a983d5
Kiel Regusters <regustersk@gmail.com>
2016-09-06 18:03:22 -0700

    Added the main class diagram that handles anything drawn, i.e. screen entities

---
+++ Strata/Screen Entities/Character.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Strata.Screen_Entities
+{
+    class Character : GameObject
+    {
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ Strata/Screen Entities/GameObject.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Strata.Screen_Entities
+{
+    public abstract class GameObject
+    {
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
