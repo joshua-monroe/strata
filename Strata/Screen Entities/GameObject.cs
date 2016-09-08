f5ec27ac1920df45747925372c3743c926031a8f
Kiel Regusters <regustersk@gmail.com>
2016-09-07 17:50:26 -0700

    Test commit to develop branch.

--- Strata/Screen Entities/GameObject.cs
+++ Strata/Screen Entities/GameObject.cs
@@ -5,6 +5,7 @@ namespace Strata.Screen_Entities
 {
     public abstract class GameObject
     {
+        //This is a comment.
         public abstract void Update(GameTime gameTime);
         public abstract void Draw(SpriteBatch spriteBatch);
     }
